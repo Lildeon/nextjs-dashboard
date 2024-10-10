65716b2b0a27aefeff0e2b075b214ccdd09a8771
Gideon Sowah <gideonsowah9@gmail.com>
2024-10-10 12:17:51 +0000

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+      <body
+        className={`${inter.className} antialiased container max-w-screen-2xl mx-auto`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
